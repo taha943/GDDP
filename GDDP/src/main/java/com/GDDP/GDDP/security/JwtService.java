3e9913e835ba44e67523341f4f703f9850934181
taha943 <tahafilali980@gmail.com>
2024-11-23 15:29:08 +0100

    GDDP-16 validate token

--- GDDP/src/main/java/com/GDDP/GDDP/security/JwtService.java
+++ GDDP/src/main/java/com/GDDP/GDDP/security/JwtService.java
@@ -4,6 +4,9 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,6 +25,25 @@ public class JwtService {
     @Value("${application.security.jwt.secret-key }")
     private String secretKey;
 
+
+    public String extractUsername(String token) {
+        return extractClaim(token,Claims::getSubject);
+    }
+
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
     public String generateToken(UserDetails userDetails ) {
         return generateToken(new HashMap<>() , userDetails);
 
@@ -51,6 +73,18 @@ public class JwtService {
     private Key getSignInKey() {
         byte[] keyBytes=Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+    public boolean isTokenValid(String token , UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token) );
+    }
+    private boolean isTokenExpired(String token) {
+
+        return extractExpiration(token).before(new Date());
+    }
+    private Date extractExpiration(String token) {
+
+        return extractClaim(token, Claims::getExpiration);
     }
 
 
