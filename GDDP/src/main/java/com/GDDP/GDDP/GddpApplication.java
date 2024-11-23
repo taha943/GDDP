570cc585e6e2322c1ba6416fbecb2b859a2272da
Taha Filali <tahafilali980@gmail.com>
2024-11-23 13:35:41 +0100

    initial commit

---
+++ GDDP/src/main/java/com/GDDP/GDDP/GddpApplication.java
@@ -0,0 +1,13 @@
+package com.GDDP.GDDP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GddpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GddpApplication.class, args);
+	}
+
+}
---
+++ GDDP/src/test/java/com/GDDP/GDDP/GddpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GDDP.GDDP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GddpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
