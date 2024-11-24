0c8a981d9ebba025dc5189db24b5a1519aa89d41
taha943 <tahafilali980@gmail.com>
2024-11-24 12:43:58 +0100

    GDDP-13

--- GDDP/src/main/java/com/GDDP/GDDP/GddpApplication.java
+++ GDDP/src/main/java/com/GDDP/GDDP/GddpApplication.java
@@ -1,7 +1,13 @@
 package com.GDDP.GDDP;
 
+import com.GDDP.GDDP.entity.Role;
+import com.GDDP.GDDP.repository.RoleRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GddpApplication {
@@ -10,4 +16,20 @@ public class GddpApplication {
 		SpringApplication.run(GddpApplication.class, args);
 	}
 
+	// Add role initialization as a CommandLineRunner bean
+	@Bean
+	public CommandLineRunner initializeRoles(RoleRepo roleRepository) {
+		return args -> {
+			String[] roles = {"USER", "ADMIN"};
+
+			for (String roleName : roles) {
+				roleRepository.findByName(roleName).orElseGet(() -> {
+					Role role = new Role();
+					role.setName(roleName);
+					role.setCreatedDate(LocalDateTime.now());
+					return roleRepository.save(role);
+				});
+			}
+		};
+	}
 }
